77c96f093f8baee444bece0294fe46753d7f6d4d
Giomar <gmunozcu@unsa.edu.pe>
2024-05-02 10:12:31 -0500

    Se elimina clase FileManager y su implementacion

--- filemanager.h
+++ filemanager.h
@@ -3,37 +3,11 @@
 
 #include <iostream>
 #include <string>
-#include <cstring>
-#include "filemanager.h"
 #include <fstream>
 #include <sstream>
 #include <filesystem>
+#include "disco.h"
 
-class FileManager
-{
-    private:
-        static const int MAX_CSV_FILES = 1000;
-        static const int MAX_COLUMNS = 1000;
-        static const int MAX_COLUMN_NAME_LENGTH = 100;
-        static const int MAX_TYPE_LENGTH = 100;
 
-        char csvFiles[MAX_CSV_FILES][MAX_COLUMN_NAME_LENGTH];
-        char columnTypes[MAX_CSV_FILES][MAX_COLUMNS][MAX_COLUMN_NAME_LENGTH];
-        char dataType[MAX_TYPE_LENGTH][MAX_TYPE_LENGTH];
-
-        int csvFilesCount;
-        int columnCount[MAX_CSV_FILES];
-
-        int findCsvFiles();
-        void displayCsvFiles() const;
-        void readColumnTypes(const std::string&);
-        void assingColumnTypes(const char columns[MAX_COLUMNS][MAX_COLUMN_NAME_LENGTH]);
-        void loadDataTypes();
-        bool isValidDataType(const char type[MAX_TYPE_LENGTH]) const;
-    public:
-        FileManager();
-        void selectCsvFile();
-        void displayColumnTypes() const;
-};
 
 #endif
