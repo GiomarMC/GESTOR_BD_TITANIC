51f350d4f76a6e8dedeb3d0759276327351d8295
Giomar <gmunozcu@unsa.edu.pe>
2024-05-13 16:24:32 -0500

    Se crea archivo file que contendra la clase File

---
+++ file.cpp
@@ -0,0 +1,18 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <filesystem>
+
+class File
+{
+    private:
+        std::string fileHead;
+        std::string typeHead;
+    public:
+        File();
+};
+
+#endif
